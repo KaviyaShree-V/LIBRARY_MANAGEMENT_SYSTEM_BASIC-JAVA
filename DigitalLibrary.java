d594ffef21f7228ab9b1dc4bc5265e978a370ad5
KaviyaShree-V <karthikavi1976@gmail.com>
2025-03-31 23:16:12 +0530

    Updated

--- DigitalLibrary.java
+++ DigitalLibrary.java
@@ -1,7 +1,9 @@
+package LibraryManagementSystem;
+
 import java.util.Scanner;
 
-public class DigitalLibrary extends Library{
-    static Scanner scanner=new Scanner(System.in);
+public class DigitalLibrary extends Library {
+    static Scanner scanner = new Scanner(System.in);
     public static void eBook(){
         System.out.println(" E-books");
         System.out.println("Did you need to read books... \n1. Yes \n2. No");
@@ -9,13 +11,13 @@ public class DigitalLibrary extends Library{
         switch (choice) {
             case 1:
                 System.out.println("Read Books...  :)");
-                System.out.println("Enter the Book or Author name to Read ");
+                System.out.println("Enter the LibraryManagementSystem.Book or Author name to Read ");
                 scanner.nextLine();
                 String name = scanner.nextLine();
-                for (Book t:book) {
-//                    Book g =t;
+                for (Book t: Library.book) {
+//                    LibraryManagementSystem.Book g =t;
                     if (t.getName().contains(name)|| t.getAuthor().contains(name)){
-                        System.out.println("Book is Opening..");
+                        System.out.println("LibraryManagementSystem.Book is Opening..");
                         System.out.println(" Start Reading :) ");
                         break;
                     }else {
