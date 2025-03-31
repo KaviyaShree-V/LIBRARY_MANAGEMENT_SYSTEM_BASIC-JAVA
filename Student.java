8459cb3cfb9f9160f90a5490d335eb54867b0d90
KaviyaShree-V <karthikavi1976@gmail.com>
2025-03-31 23:18:04 +0530

    Updated

--- Student.java
+++ Student.java
@@ -1,28 +1,28 @@
-import java.util.ArrayList;
+package LibraryManagementSystem;
 
 public class Student {
     private String name;
     private String regNo;
 
-    private static ArrayList<Student> studentDetails=new ArrayList<>();
-
-    static {
-        studentDetails.add(new Student("S1","sp1"));
-    }
+//    private static ArrayList<LibraryManagementSystem.ACTIONS> studentDetails = new ArrayList<>();
+//
+//    static {
+//        studentDetails.add(new LibraryManagementSystem.ACTIONS("S1","sp1"));
+//    }
     public Student(String name,String regNo){
         this.name=name;
         this.regNo=regNo;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getRegNo() {
-        return regNo;
-    }
-
-    public static ArrayList<Student> getStudentDetails() {
-        return studentDetails;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getRegNo() {
+//        return regNo;
+//    }
+//
+//    public static ArrayList<LibraryManagementSystem.ACTIONS> getStudentDetails() {
+//        return studentDetails;
+//    }
 }
