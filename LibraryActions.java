ac4ad0528dc3b4d730db7b1274286222cf08f5c4
KaviyaShree-V <karthikavi1976@gmail.com>
2025-04-01 23:35:12 +0530

    Updated

--- LibraryActions.java
+++ LibraryActions.java
@@ -5,18 +5,21 @@ import java.util.Scanner;
 public class LibraryActions extends Library {
     static Scanner scanner = new Scanner(System.in);
 
+    //method for login
     public static void login() {
-        System.out.println("1. Student Login \n2. Staff Login");
+        System.out.println("1. Student Login \n2. Staff Login \n3. Librarian Login");
         System.out.println("Enter the choice: ");
         int choice = scanner.nextInt();
+        //checking the login cases
         switch (choice) {
             case 1:
-                StudentActions studentActions = new StudentActions();
-                studentActions.studentLogin();
+                CommonActions.login();
                 break;
             case 2:
-                LibrarianActions librarianActions = new LibrarianActions();
-                librarianActions.librarianLogin();
+                CommonActions.login();
+                break;
+            case 3:
+                CommonActions.login();
                 break;
             default:
                 System.out.println("Enter the valid choice: ");
@@ -24,9 +27,10 @@ public class LibraryActions extends Library {
         }
     }
 
+    //method to display choices or actions to perform
     public static void enterChoice() {
         int choice = 0;
-        while (choice != 9) {
+        while (choice != 9) {//Continuous menu until exit
             System.out.println("Enter a choice to perform an action:");
             System.out.println(" 1. ADD BOOKS \n 2. SEARCH BOOKS \n 3. REMOVE BOOKS \n 4. BORROW BOOKS \n 5. E-book \n 6. View Books \n 7.View All Books \n 8. Add Students & Librarians \n 9. EXIT");
             choice = scanner.nextInt();
@@ -41,17 +45,17 @@ public class LibraryActions extends Library {
                     int r = scanner.nextInt();
                     if (r == 1) {
                         System.out.println("Searching Books by Title....");
-                        searchBoooks();
+                        CommonActions.searchBoooks();
                     } else if (r == 2) {
                         System.out.println("Searching Books by Title....");
                         System.out.println("Enter the Author Name:");
                         scanner.nextLine();
                         String bn = scanner.nextLine();
-                        searchBoooks(bn);
+                        CommonActions.searchBoooks(bn);
                     } else {
                         System.out.println("Enter a valid choice");
                     }
-                    searchBoooks();
+                    CommonActions.searchBoooks();
                     break;
                 case 3:
                     System.out.println("Remove Books.....");
@@ -59,7 +63,7 @@ public class LibraryActions extends Library {
                     break;
                 case 4:
                     System.out.println("Borrow Books.....");
-                    borrowBooks();
+                    CommonActions.borrowBooks();
                     break;
                 case 5:
                     System.out.println("E-book.....");
@@ -67,11 +71,11 @@ public class LibraryActions extends Library {
                     break;
                 case 6:
                     System.out.println("View Books");
-                    viewBooks();
+                    CommonActions.viewBooks();
                     break;
                 case 7:
                     System.out.println("View All Books....");
-                    viewAllBooks();
+                    CommonActions.viewAllBooks();
                     break;
                 case 8:
                     System.out.println("Adding students/Librarians..");
@@ -93,148 +97,6 @@ public class LibraryActions extends Library {
                 default:
                     System.out.println("Enter a Valid Choice:");
             }
-        }
-    }
-    public static void searchBoooks()
-    {
-        System.out.println("Enter a Book name :");
-        scanner.nextLine();
-        String name= scanner.nextLine();
-        for (int i=0;i<getBook().size();i++)
-        {
-            Book b=getBook().get(i);
-            if (b.getName().equals(name))
-                {
-                    System.out.println("NAME :" + b.getName());
-                    System.out.println("AUTHOR: " + b.getAuthor());
-                    System.out.println("ISBN :" + b.getIsbn());
-                    System.out.println("Edition: "+b.getEdition());
-                    System.out.println("Available Stock : "+b.getCount());
-                    System.out.println("Did you need to Borrow / Remove the Book : \n1. Yes \n2. No");
-                    int c= scanner.nextInt();
-                    if (c==1)
-                    {
-                        System.out.println("1. Borrow \n2. Remove ");
-                        int choice = scanner.nextInt();
-                        switch (choice)
-                        {
-                            case 1:
-                                System.out.println("Borrow Books");
-                                borrowBooks();
-                                break;
-                            case 2:
-                                System.out.println("Remove Books");
-                                LibrarianActions.removeBooks();
-                                break;
-                            default:
-                                System.out.println("Thanks For Visiting....");
-                                System.exit(3);
-                        }
-                    }else
-                    {
-                        System.out.println("No Such Book Found In The Library");
-                    }
-                    break;
-                }
-
-            if (getBook().isEmpty())
-            {
-                System.out.println("No books Available");
-            }
-        }
-    }
-
-    public static void searchBoooks(String authorName)
-    {
-        for (Book i  : book)
-        {
-            if (i.getAuthor().equals(authorName))
-            {
-                System.out.println("NAME :" + i.getName());
-                System.out.println("AUTHOR: " + i.getAuthor());
-                System.out.println("ISBN :" + i.getIsbn());
-                System.out.println("Edition: "+i.getEdition());
-                System.out.println("Available Stock : "+i.getCount());
-                System.out.println("Did you need to Borrow / Remove the Book : \n1. Yes \n2. No");
-                int c= scanner.nextInt();
-                if (c==1)
-                {
-                    System.out.println("1. Borrow \n2. Remove ");
-                    int choice = scanner.nextInt();
-                    switch (choice)
-                    {
-                        case 1:
-                            System.out.println("Borrow Books");
-                            borrowBooks();
-                            break;
-                        case 2:
-                            System.out.println("Remove Books");
-                            LibrarianActions.removeBooks();
-                            break;
-                        default:
-                            System.out.println("Thanks For Visiting....");
-                            System.exit(3);
-                    }
-                }break;
-            }else {
-                System.out.println("No Such Book Found In The Library");
-            }
-            if (getBook().isEmpty())
-            {
-                System.out.println("No books Available");
-            }
-        }
-    }
-
-    public static void viewAllBooks(){
-        System.out.println("Viewing Books...");
-        for (Book book1:book){
-            System.out.println("Book Name: "+book1.getName());
-            System.out.println("Author Name: "+book1.getAuthor());
-            System.out.println("Isbn : "+book1.getIsbn());
-            System.out.println("Edition: "+book1.getEdition());
-            System.out.println("Stock :"+book1.getCount());
-        }
-    }
-
-    public static void viewBooks(){
-        System.out.println("Enter the specific book name to View :");
-        scanner.nextLine();
-        String b1= scanner.nextLine();
-        for (Book b:Library.getBook()){
-            if (b.getName().contains(b1)){
-                System.out.println("Book Name: "+b.getName());
-                System.out.println("Author Name: "+b.getAuthor());
-                System.out.println("Isbn : "+b.getIsbn());
-                System.out.println("Edition: "+b.getEdition());
-                System.out.println("Stock :"+b.getCount());
-            }
-        }
-    }
-
-    public static void borrowBooks() {
-        System.out.println("Enter your Name:");
-        String name = scanner.next();
-        System.out.println("Enter your Mobile Number:");
-        String mob = scanner.next();
-        System.out.println("Enter the Name of the Book:");
-        scanner.nextLine();
-        String boo = scanner.nextLine();
-        boolean bookfound = false;
-        for (int u = 0; u < book.size(); u++) {
-            Book v = getBook().get(u);
-            if (v.getName().contains(boo)) {
-                System.out.println("Enter the Number books to Borrow :");
-                int borrow = Integer.parseInt(scanner.nextLine());
-                System.out.println("The Book is Registered for the Reader " + name + " and you should return or Renew the book " + boo + "within 07 days");
-                int x= v.getCount();
-                int y= x-borrow;
-                v.setCount(y);
-                bookfound = true;
-                break;
-            }
-            System.out.println("There is no book matches in your need");
-            break;
         }
     }
 }
