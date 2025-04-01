af960fdbb4d180408423739a62f67cd605bfd1c7
KaviyaShree-V <karthikavi1976@gmail.com>
2025-04-01 23:13:33 +0530

    Book

--- Book.java
+++ Book.java
@@ -1,15 +1,15 @@
 package LibraryManagementSystem;
 
-import java.util.Scanner;
-
 public class Book {
-    static Scanner scanner=new Scanner(System.in);
+    //Fields
     private String name;
     private String author;
     private String isbn;
     private int edition;
     private int count;
     private boolean status;
+
+    // Constructor to create a new Book instance.
     public Book(String title,String author, String isbn,int edition,int count, boolean status){
         this.name=title;
         this.author=author;
@@ -18,6 +18,8 @@ public class Book {
         this.count=count;
         this.status=status;
     }
+    // Getter methods
+
     public String getName() {
         return name;
     }
@@ -29,17 +31,26 @@ public class Book {
     public String getIsbn() {
         return isbn;
     }
+
     public int getEdition() {
         return edition;
     }
+
     public int getCount() {
         return count;
     }
+
+    // Setter method
+
     public int setCount(int c){
         this.count=c;
         return count;
     }
+
+    //Main method - calls LibraryActions.login()
+
     public static void main(String[] args) {
         LibraryActions.login();
     }
+
 }
