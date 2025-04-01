0af31fd389c16f079ff786c770de31fa08b02d6e
KaviyaShree-V <karthikavi1976@gmail.com>
2025-04-01 23:35:33 +0530

    Update

---
+++ Staff.java
@@ -0,0 +1,12 @@
+package LibraryManagementSystem;
+
+public class Staff {
+    private String name;
+    private String regNo;
+
+    public Staff(String name,String regNo){
+        this.name=name;
+        this.regNo=regNo;
+    }
+
+}
---
+++ StaffActions.java
@@ -0,0 +1,24 @@
+package LibraryManagementSystem;
+
+import java.util.Scanner;
+
+public class StaffActions {
+
+//    Scanner scanner=new Scanner(System.in);
+//    public void stafflogin() {
+//        System.out.println("Welcome to the Library");
+//        System.out.println("Enter your Name: ");
+//        String name = scanner.nextLine();
+//        System.out.println("Enter your getId: ");
+//        String id = scanner.nextLine();
+//        for (Actions librarian : Actions.getDetails()) {
+//            if (librarian.getName().equals(name) && librarian.getId().equals(id)) {
+//                System.out.println("Login Successful!");
+//                LibraryActions.enterChoice();
+//                return;
+//            }
+//        }
+//
+//        System.out.println("Login Unsuccessful! Try Again");
+//    }
+}
