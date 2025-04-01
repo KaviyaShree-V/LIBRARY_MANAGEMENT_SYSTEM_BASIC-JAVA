fbf40db79a8b5ee964d02502c407e2dd9a406576
KaviyaShree-V <karthikavi1976@gmail.com>
2025-04-01 23:35:48 +0530

    Update

--- StudentActions.java
+++ StudentActions.java
@@ -3,23 +3,23 @@ package LibraryManagementSystem;
 import java.util.Scanner;
 
 public class StudentActions{
-
-    Scanner scanner=new Scanner(System.in);
-
-    public void studentLogin(){
-        System.out.println("Welcome to the LibraryManagementSystem.Library");
-        System.out.println("Enter your Name: ");
-        scanner.nextLine();
-        String name= scanner.nextLine();
-        System.out.println("Enter your Regno.: ");
-        String regNo = scanner.nextLine();
-        for (var student: ACTIONS.getDetails()) {
-            if (student.getName().equals(name) && student.getId().equals(regNo)) {
-                LibraryActions.enterChoice();
-            } else {
-                System.out.println("Login Unsuccessful!  Try Again");
-                return;
-            }
-        }
-    }
+//
+//    Scanner scanner=new Scanner(System.in);
+//
+//    public void studentLogin(){
+//        System.out.println("Welcome to the Library");
+//        System.out.println("Enter your Name: ");
+//        scanner.nextLine();
+//        String name= scanner.nextLine();
+//        System.out.println("Enter your Regno.: ");
+//        String regNo = scanner.nextLine();
+//        for (var student: Actions.getDetails()) {
+//            if (student.getName().equals(name) && student.getId().equals(regNo)) {
+//                LibraryActions.enterChoice();
+//            } else {
+//                System.out.println("Login Unsuccessful!  Try Again");
+//                return;
+//            }
+//        }
+//    }
 }
