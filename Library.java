4e606f861c0e241686c44bb859e1cbe77b40da2a
KaviyaShree-V <karthikavi1976@gmail.com>
2025-04-01 23:13:53 +0530

    Library - Pojo

--- Library.java
+++ Library.java
@@ -3,13 +3,16 @@ package LibraryManagementSystem;
 import java.util.ArrayList;
 
 public class Library {
+    //Fields
     private String name;
     private String author;
-
+    //Arraylist to store the books list
     static ArrayList<Book> book = new ArrayList<>();
+    //static initializer
     static {
         book.add(new Book("Wings of Fire", "Dr. A.P.J. Abdul Kalam", "wof0010", 1, 6, true));
     }
+    //getter
     public static ArrayList<Book> getBook() {
         return book;
     }
