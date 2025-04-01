475845bc2cc4240d88ccee9cd1da56819d5f91b3
KaviyaShree-V <karthikavi1976@gmail.com>
2025-04-01 23:36:03 +0530

    Student

--- Student.java
+++ Student.java
@@ -4,25 +4,9 @@ public class Student {
     private String name;
     private String regNo;
 
-//    private static ArrayList<LibraryManagementSystem.ACTIONS> studentDetails = new ArrayList<>();
-//
-//    static {
-//        studentDetails.add(new LibraryManagementSystem.ACTIONS("S1","sp1"));
-//    }
     public Student(String name,String regNo){
         this.name=name;
         this.regNo=regNo;
     }
 
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getRegNo() {
-//        return regNo;
-//    }
-//
-//    public static ArrayList<LibraryManagementSystem.ACTIONS> getStudentDetails() {
-//        return studentDetails;
-//    }
 }
